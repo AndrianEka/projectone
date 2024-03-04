16d885ebdb3a49135f7eab2e9be766c190b8c3b3
Andrian <andrianeka@hotmail.com>
2024-03-04 22:30:58 +0700

    modified:   app/Http/Controllers/PelangganController.php 	modified:   resources/views/pelanggan/create.blade.php

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -9,22 +9,31 @@ use Termwind\Components\Dd;
 
 class PelangganController extends Controller
 {
-   public function index(){
+  public function index()
+  {
     $pelanggan = Pelanggan::all();
-    return view('pelanggan.index',compact(['pelanggan']));
-   }
+    return view('pelanggan.index', compact(['pelanggan']));
+  }
 
-   public function create(){
+  public function create()
+  {
     return view('pelanggan.create');
-   }
+  }
 
-    public function store(Request $request){
+  public function store(Request $request)
+  {
+    $request->validate([
+      'nama' => 'required',
+      'alamat' => 'required',
+      'no_hp' => 'required',
+      'keterangan' => 'required'
+    ]);
     Pelanggan::create([
-      'nama'=>$request->nama,
-      'alamat'=>$request->alamat,
-      'no_hp'=>$request->no_hp,
-      'keterangan'=>$request->keterangan
+      'nama' => $request->nama,
+      'alamat' => $request->alamat,
+      'no_hp' => $request->no_hp,
+      'keterangan' => $request->keterangan
     ]);
     return Redirect('/pelanggan');
-   }
+  }
 }
